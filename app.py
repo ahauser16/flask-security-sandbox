e9bdd1664bf597f0d1a684257f1dccf94dfbfce4
Arthur Hauser <arthur.hauser@gmail.com>
2024-05-19 20:02:12 -0400

    add table and addNotarialAct form but db needs refactor

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify, session
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    request,
+    jsonify,
+    session,
+    abort,
+)
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import (
     UserMixin,
@@ -67,6 +76,50 @@ class NotaryCredentials(db.Model):
     user = db.relationship("User", backref="notary_credentials_backref", uselist=False)
 
 
+class NotarialAct(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    date = db.Column(db.DateTime)
+    time = db.Column(db.Time)
+    act_type = db.Column(db.String(100))
+    other_act_type_input = db.Column(db.String(100), nullable=True)
+    principal_name = db.Column(db.String(100))
+    principal_addressLine1 = db.Column(db.String(100))
+    principal_addressLine2 = db.Column(db.String(100))
+    principal_city = db.Column(db.String(100))
+    principal_state = db.Column(db.String(2))
+    principal_zipCode = db.Column(db.String(100))
+    service_number = db.Column(db.Integer)
+    service_type = db.Column(db.String(100))
+    credential_type = db.Column(db.String(100))
+    communication_tech = db.Column(db.String(100), nullable=True)
+    certification_authority = db.Column(db.String(100), nullable=True)
+    verification_provider = db.Column(db.String(100), nullable=True)
+    user = db.relationship("User", backref=db.backref("notarial_acts", lazy=True))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "date": self.date,
+            "time": self.time,
+            "act_type": self.act_type,
+            "other_act_type_input": self.other_act_type_input,
+            "principal_name": self.principal_name,
+            "principal_addressLine1": self.principal_addressLine1,
+            "principal_addressLine2": self.principal_addressLine2,
+            "principal_city": self.principal_city,
+            "principal_state": self.principal_state,
+            "principal_zipCode": self.principal_zipCode,
+            "service_number": self.service_number,
+            "service_type": self.service_type,
+            "credential_type": self.credential_type,
+            "communication_tech": self.communication_tech,
+            "certification_authority": self.certification_authority,
+            "verification_provider": self.verification_provider,
+        }
+
+
 user_datastore = SQLAlchemySessionUserDatastore(db.session, User, Role)
 security = Security(app, user_datastore)
 
@@ -76,9 +129,74 @@ def create_tables():
     db.create_all()
 
 
+# @app.before_first_request
+# def reset_database():
+#     db.drop_all()
+#     db.create_all()
+
+
 @app.route("/")
 def index():
     return render_template("base.html")
+
+
+############################# new code below
+
+
+@app.route("/api/notarial_act_data")
+def notarial_act_data():
+    query = NotarialAct.query.filter_by(user_id=current_user.id)
+
+    # search filter
+    search = request.args.get("search")
+    if search:
+        query = query.filter(NotarialAct.act_type.like(f"%{search}%"))
+
+    total = query.count()
+
+    # sorting
+    sort = request.args.get("sort")
+    if sort:
+        order = []
+        for s in sort.split(","):
+            direction = s[0]
+            name = s[1:]
+            if name not in ["date", "time", "act_type"]:
+                name = "date"
+            col = getattr(NotarialAct, name)
+            if direction == "-":
+                col = col.desc()
+            order.append(col)
+        if order:
+            query = query.order_by(*order)
+
+    # pagination
+    start = request.args.get("start", type=int, default=-1)
+    length = request.args.get("length", type=int, default=-1)
+    if start != -1 and length != -1:
+        query = query.offset(start).limit(length)
+
+    # response
+    return {
+        "data": [act.to_dict() for act in query],
+        "total": total,
+    }
+
+
+@app.route("/api/notarial_act_data", methods=["POST"])
+def update_notarial_act():
+    data = request.get_json()
+    if "id" not in data:
+        abort(400)
+    act = NotarialAct.query.get(data["id"])
+    for field in ["date", "time", "act_type"]:
+        if field in data:
+            setattr(act, field, data[field])
+    db.session.commit()
+    return "", 204
+
+
+############################ new code above
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -284,7 +402,41 @@ def mydetails():
 @app.route("/notarylogbook")
 @roles_accepted("Admin", "Traditional Notary", "Electronic Notary")
 def notarylogbook():
-    return render_template("mynotarylogbook.html")
+    return render_template(
+        "mynotarylogbook.html", notarial_acts=current_user.notarial_acts
+    )
+
+
+@app.route("/addnotarylogentry", methods=["GET", "POST"])
+def add_notary_log_entry():
+    if request.method == "POST":
+        act = NotarialAct(
+            date=request.form.get("date"),
+            time=request.form.get("time"),
+            act_type=request.form.get("act_type"),
+            other_act_type_input=request.form.get("other_act_type_input"),
+            principal_name=request.form.get("principal_name"),
+            principal_addressLine1=request.form.get("principal_addressLine1"),
+            principal_addressLine2=request.form.get("principal_addressLine2"),
+            principal_city=request.form.get("principal_city"),
+            principal_state=request.form.get("principal_state"),
+            principal_zipCode=request.form.get("principal_zipCode"),
+            service_number=request.form.get("service_number"),
+            service_type=request.form.get("service_type"),
+            credential_type=request.form.get("credential_type"),
+            communication_tech=request.form.get("communication_tech"),
+            certification_authority=request.form.get("certification_authority"),
+            verification_provider=request.form.get("verification_provider"),
+        )
+        db.session.add(act)
+        db.session.commit()
+    return render_template("addnotarialactform.html")
+
+
+@app.route("/findnotary")
+# @roles_accepted("Admin", "Principal", "Traditional Notary", "Electronic Notary")
+def findnotary():
+    return render_template("findnotary.html")
 
 
 @app.route("/myesignature")
